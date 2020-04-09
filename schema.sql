5cbede49437fb40bacbd2726416c65e2c63a19b1
GARFAM <garfam2@mail.ru>
2020-04-09 13:38:26 +0300

    sixth task

---
+++ schema.sql
@@ -0,0 +1,59 @@
+-----------------Таблицы-----------------
+CREATE TABLE `bets` (
+  `id` int(11) AUTO_INCREMENT,
+  `bet_date` datetime NOT NULL,
+  `bet_price` decimal(10,0) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `lot_id` int(11) NOT NULL,
+  PRIMARY KEY(`id`)
+);
+CREATE TABLE `categories` (
+  `id` int(11) AUTO_INCREMENT,
+  `title` varchar(45),
+  `allas` varchar(15),
+  PRIMARY KEY(`id`,`title`,`allas`)
+);
+CREATE TABLE `lots` (
+  `id` int(11) AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `date_create` datetime NOT NULL,
+  `start_price` decimal(10,0) NOT NULL,
+  `image_url` varchar(100) NOT NULL,
+  `end_date` datetime NOT NULL,
+  `bet_step` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `winner_id` int(11),
+  `bet_price` int(11),
+  PRIMARY KEY(`id`)
+);
+
+CREATE TABLE `users` (
+@@ -39,7 +39,7 @@ CREATE TABLE `users` (
+  `avatar` varchar(100) NOT NULL,
+  `reg_date` datetime NOT NULL,
+  `contacts` varchar(15) NOT NULL,
+  PRIMARY KEY(`id`,`username`,`email`)
+);
+
+-----------------Индексы-----------------
+ALTER TABLE `bets`
+  ADD KEY `bet_price` (`bet_price`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `lot_id` (`lot_id`);
+ALTER TABLE `lots`
+  ADD KEY `start_price` (`start_price`),
+  ADD KEY `bet_step` (`bet_step`),
+  ADD KEY `category_id` (`category_id`),
+  ADD KEY `bet_price` (`bet_price`),
+  ADD KEY `user_id` (`user_id`,`winner_id`),
+  ADD KEY `winner_id` (`winner_id`);
+-----------------Связи-----------------
+ALTER TABLE `bets`
+  ADD CONSTRAINT `ставки_на_лот` FOREIGN KEY (`lot_id`) REFERENCES `lots` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `lots`
+  ADD CONSTRAINT `категория_лота` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `владелец_лота` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `победитель_лота` FOREIGN KEY (`winner_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
